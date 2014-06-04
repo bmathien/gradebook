c26ef39d70138e89fee73a95d56b922c2a8fb88e
Brian Mathien <bmathien@barrington220.org>
2014-06-04 10:29:24 -0500

    Initial Commit

---
+++ gradebook/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gradebook
+//
+//  Created by teacher on 6/4/14.
+//  Copyright (c) 2014 Mathien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ gradebookTests/gradebookTests.m
@@ -0,0 +1,34 @@
+//
+//  gradebookTests.m
+//  gradebookTests
+//
+//  Created by teacher on 6/4/14.
+//  Copyright (c) 2014 Mathien. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface gradebookTests : XCTestCase
+
+@end
+
+@implementation gradebookTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
